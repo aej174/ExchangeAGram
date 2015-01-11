6c2691e00b4116195d194e16ef547354de16cb0f
aej174 <aej174@icloud.com>
2015-01-11 09:23:17 -0500

    first commit

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Allan Jones on 1/6/15.
+//  Copyright (c) 2015 Allan Jones. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+}
